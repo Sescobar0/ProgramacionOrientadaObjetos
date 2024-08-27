8b12ae2fee78836fdbe5759b0e8396aeaeb5eab2
Sescobar0 <148839742+Sescobar0@users.noreply.github.com>
2024-08-27 01:15:17 +0000

    Ejercicios 26/08/2024

---
+++ Clases/Computador.java
@@ -0,0 +1,42 @@
+package Clases;
+
+public class Computador {
+
+    //Atributos de la clase
+    private String marca;
+    private String color;
+    private String modelo;
+    private int serie;
+    private double pulgadasPantalla;
+
+    //Metodo Constructor de la clase 
+    public Computador(String marca, String color, String modelo, int serie, double pulgadasPantalla) {
+        this.marca = marca;
+        this.color = color;
+        this.modelo = modelo;
+        this.serie = serie;
+        this.pulgadasPantalla = pulgadasPantalla;
+
+
+    //Metodos de la clase
+    public boolean encender(){
+        return true;
+    }
+
+    public boolean apagar(){
+        return true;
+    }
+    
+    public void reubicar(String oficina){
+        System.out.println("El computador de marca " + marca + " se reubico en la oficina " + oficina );
+    }
+
+    public void asignar(String trabajador){
+        System.out.println("El computador con la serie " + serie + " fue asignado a: " + trabajador);
+    }
+
+    public String toString(){
+        return "Computador { marca: " + marca + " color: " + color + " modelo: " + modelo + " serie: " + serie + " pulgadas: " + pulgadasPantalla + " }";
+    }
+
+}
---
+++ Clases/Ejecutar.java
@@ -0,0 +1,17 @@
+
+import Clases.Computador;
+
+//package Clases;
+
+public class Ejecutar {
+
+    public static void main(String[] args) {
+        
+        Computador objComputador1;
+        objComputador1 = new Computador("HP", "Negro", "MLX32342", 021610, 21.0);
+
+        Computador objComputador2 = new Computador("Lenovo", "Gris", "LMNR69", 044563, 15.0);
+        
+    }
+    
+}
