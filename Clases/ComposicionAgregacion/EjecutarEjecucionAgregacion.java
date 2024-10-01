d7e933f6363358e2bf0dcc067de5541a8678cc79
Sescobar0 <148839742+Sescobar0@users.noreply.github.com>
2024-10-01 01:33:15 +0000

    fial

--- Clases/ComposicionAgregacion/EjecutarEjecucionAgregacion.java
+++ Clases/ComposicionAgregacion/EjecutarEjecucionAgregacion.java
@@ -5,9 +5,7 @@ import java.time.LocalDateTime;
 public class EjecutarEjecucionAgregacion {
 
     public static void main(String[] args) {
-        
-
-        
+ 
         LocalDateTime fechaHoraEspecifica = LocalDateTime.of(2024, 10, 15, 10, 30);
         System.out.println("Fecha y hora específica: " + fechaHoraEspecifica);
         Cita objCita1 = new Cita(fechaHoraEspecifica, "Oftalmólogo");
