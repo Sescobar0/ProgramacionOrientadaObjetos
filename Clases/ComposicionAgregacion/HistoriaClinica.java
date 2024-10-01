53f5214bad348a7de583e4c35a118f4ecf3b173e
Sescobar0 <148839742+Sescobar0@users.noreply.github.com>
2024-10-01 00:31:54 +0000

    Clasenueva

---
+++ Clases/ComposicionAgregacion/HistoriaClinica.java
@@ -0,0 +1,40 @@
+package Clases.ComposicionAgregacion;
+
+public class HistoriaClinica {
+    
+    private String anamnesis;
+    private String formulaOptica;
+    private String medicamento;
+    
+    public HistoriaClinica(String anamnesis, String formulaOptica, String medicamento) {
+        this.anamnesis = anamnesis;
+        this.formulaOptica = formulaOptica;
+        this.medicamento = medicamento;
+    }
+
+    public String getAnamnesis() {
+        return anamnesis;
+    }
+
+    public void setAnamnesis(String anamnesis) {
+        this.anamnesis = anamnesis;
+    }
+
+    public String getFormulaOptica() {
+        return formulaOptica;
+    }
+
+    public void setFormulaOptica(String formulaOptica) {
+        this.formulaOptica = formulaOptica;
+    }
+
+    public String getMedicamento() {
+        return medicamento;
+    }
+
+    public void setMedicamento(String medicamento) {
+        this.medicamento = medicamento;
+    } 
+
+    
+}
---
+++ Clases/ComposicionAgregacion/Paciente.java
@@ -0,0 +1,5 @@
+package Clases.ComposicionAgregacion;
+
+public class Paciente {
+    
+}
